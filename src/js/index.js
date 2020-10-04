93c75036d19a5e522309050ee27d717112b51615
Douglas <dodosobreira@gmail.com>
2020-10-04 00:08:23 -0300

    adicionando botao para cadastro do produto

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,15 @@ function checkLogin() {
 
     if (localStorage.getItem('userId')) { 
         $("#inicio").hide();
+        console.log($("#cadastrar-produtos").show())
+        $("#cadastrar-produtos").show()
         setTimeout(function(){
             $("#loader").hide();
             $("#exibe-itens").show();
         }, 1000);
     } else {
         $("#exibe-itens").hide();
+        $("#cadastrar-produtos").hide();
         setTimeout(function(){
             $("#loader").hide();
             $("#inicio").show();
